a1312172f78f3b51e80626cafb71593126eae0ec
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-03-31 19:15:31 +0100

    add firebase

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import { App } from "./app";
+import { FirebaseContext } from "./context/firebase";
+
+const config = {
+  apiKey: "AIzaSyCCSJJcYauARP5UYQWpDOmIx--qYPMrCyU",
+  authDomain: "netflix-ca0bb.firebaseapp.com",
+  projectId: "netflix-ca0bb",
+  storageBucket: "netflix-ca0bb.appspot.com",
+  messagingSenderId: "585595256826",
+  appId: "1:585595256826:web:5c98b402e5facb3f4aee43",
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
-
   document.getElementById("root")
 );
